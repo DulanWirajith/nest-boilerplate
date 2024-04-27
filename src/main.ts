257087cdd1b8e12ce0b853bb9444efe9eb665263
dulanwirajith <dulanwirajith1995@gmail.com>
2024-04-27 18:35:04 +0800

    Add CORS configuration and global pipes setup for enhanced application security and data validation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { CheckPayloadNotEmptyPipe } from './utils/pipes/check-payload-not-empty.pipe';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  enableCors(app);
+  useGlobalPipes(app);
+
   const port = 4000;
   await app.listen(port);
   logger.log(`Application is running on: ${await app.getUrl()}`);
+}
+
+function enableCors(app) {
+  app.enableCors({
+    allowedHeaders: ['content-type', 'authorization'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: ['http://localhost:8090'],
+    credentials: true,
+  });
+}
+
+function useGlobalPipes(app) {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+    new CheckPayloadNotEmptyPipe(),
+  );
 }
 
 bootstrap();
